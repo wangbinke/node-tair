48966a7637b8bc0cae9365b2e98b654b8e217a71
sunfang1cn <sunfang1cn@yahoo.com.cn>
2012-07-31 13:30:55 +0800

    use cluster to benchmark

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,4 +1,4 @@
-var cli = require('./../index');
+var cluster = require('cluster');
 var bench = {};
 
 var content = 'lzstat_uv=9515746301159965720|2592155@2837774@2738597@12' +
@@ -6,103 +6,99 @@ var content = 'lzstat_uv=9515746301159965720|2592155@2837774@2738597@12' +
   '1505@2857556@2059594@2208862@2581747@2692646@2751004@2490638@2852799@2735862@273' +
   '5864@2229691@2558276@2800965@2581759@2836758@2920265@2910057@2285988';
 
-bench.get = function (times) {
-  var nvcount = 0;
+var costTime = 0;
+var errCount = 0;
+
+bench.set = function (maxTimes) {
   var ccount = 0;
+  var cli = require('./../index');
   cli.initClient('group_ju', [
     {host: '10.235.144.116', port: 5198}
   ], function (err) {
     if (err) {
       console.log(err);
     }
-
-    cli.get('hello2test', function (err, data) {
-      console.time(times + ' times get');
-      for (var i = 0; i < times; i++) {
-        cli.get('hello2test', function (err, data) {
-          ccount++;
-          if (err) {
-            console.error(err);
-            nvcount++;
-          }
-          if (data !== content) {
-            console.log('not valid!!');
-            nvcount++;
-          }
-          if (ccount === times) {
-            console.timeEnd(times + ' times get');
-            console.log('Not Valid: ' + nvcount);
-            process.exit(0);
-          }
-        });
-      }
-    });
-  });
-}
-
-bench.set = function (times) {
-  var ccount = 0;
-  var notSucc = 0;
-  cli.initClient('group_ju', [
-    {host: '10.235.144.116', port: 5198}
-  ], function (err) {
-    if (err) {
-      console.log(err);
-    }
-    console.time(times + ' times set');
-    for (var i = 0; i < times; i++) {
-      cli.set('hello2test', content, function (err, succ) {
-        ccount++;
-        if (err || !succ) {
-          console.error(err);
-          notSucc++;
-        }
-        if (ccount === times) {
-          console.timeEnd(times + ' times set');
-          console.log('Not Success: ' + notSucc);
-          process.exit(0);
-        }
-      });
-    }
-  });
-}
-
-bench.cget = function () {
-  cli.initClient('group_ju', [
-    {host: '10.235.144.116', port: 5198}
-  ], function (err) {
-    if (err) {
-      console.log(err);
-    }
-    getForMem(0, new Date().getTime());
+    worker(0, new Date().getTime(), maxTimes, 'set', cli);
   });
 };
 
-function getForMem (times, starttime) {
-  cli.get('hello2test', function (err, data) {
+bench.get = function (maxTimes) {
+  var cli = require('./../index');
+  cli.initClient('group_ju', [
+    {host: '10.235.144.116', port: 5198}
+  ], function (err) {
+    if (err) {
+      console.log(err);
+    }
+    worker(0, new Date().getTime(), maxTimes, 'get', cli);
+  });
+};
+
+function worker (times, starttime, maxTimes, method, cli) {
+  var arg2 = 0;
+  if (method === 'set') {
+    arg2 = content;
+  }
+  cli[method]('hello2test', arg2, function (err, data) {
     if (err) {
       console.log(err);
     }
     times = times || 1;
     times++;
-    if (data !== content) {
-      console.log('not valid!!');
+    if (data !== content && data !== true) {
+      if (!cluster.isMaster) {
+        process.send('err');
+      }
     }
-    if (times % 1000 == 0) {
-      console.log(times + ': ' + (new Date().getTime() - starttime));
+
+    if (times === maxTimes) {
+      if (!cluster.isMaster) {
+        costTime = (new Date().getTime() - starttime);
+        process.send(costTime);
+        cluster.worker.destroy();
+      }
     }
-    getForMem(times, starttime);
+    worker(times, starttime, maxTimes, method, cli);
   });
 }
 
 function main (argv) {
   var test = argv[2];
-  var times = argv[3] || 100;
+  var times = argv[3] || 1000;
+  var threads = argv[4] || 5;
   if (!bench[test]) {
-    console.log('usage: node benchmark.js benchname [times=1000]');
+    console.log('usage: node benchmark.js benchname [times=100] [threads=5]');
     return process.exit(0);
   }
-  bench[test](parseInt(times, 10));
+  if (cluster.isMaster) {
+    console.log('Node-Tair Benchmark Started, Waiting...');
+    threads = parseInt(threads, 10);
+    for (var i = 0; i < threads; i++) {
+      cluster.fork();
+    }
+    var finished = 0;
+    Object.keys(cluster.workers).forEach(function (id) {
+      cluster.workers[id].on('message', function (msg) {
+        if (msg === 'err') {
+          errCount++;
+        } else if (!isNaN(parseInt(msg, 10))) {
+          costTime += parseInt(msg, 10);
+        }
+      });
+    });
+    cluster.on('exit', function (worker, code, signal) {
+      finished++;
+      if (finished === threads) {
+        console.log('Node-Tair Benchmark Finished.');
+        var qps = (threads * times / Math.floor(costTime / threads, 10) * 1000).toFixed(2);
+        console.log('Working Threads: %d\nRequest Counts: %d\nSuccess: %d\nFailure: %d\nCost Time: %d [ms]\nQPS: %d [#/sec]',
+          threads, threads * times, threads * times - errCount, errCount, Math.floor(costTime / threads, 10), qps
+        );
+      }
+    });
+  } else {
+    bench[test](parseInt(times, 10));
+  }
 }
 
 main(process.argv);
